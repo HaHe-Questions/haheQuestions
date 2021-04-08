9d5e702ac869c3f03bb743e646ebd7f89e405ff9
haikess <essi.haikka@myy.haaga-helia.fi>
2021-04-08 11:44:15 +0300

    kyselycontroller ja -repo

--- src/main/java/hh/fi/swd22/haheQuestions/domain/KyselyRepository.java
+++ src/main/java/hh/fi/swd22/haheQuestions/domain/KyselyRepository.java
@@ -1,7 +1,12 @@
 package hh.fi.swd22.haheQuestions.domain;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
+
 public interface KyselyRepository extends CrudRepository<Kysely, Long> {
+	
+	List<Kysely> findByNimi(String nimi);
 
 }
--- src/main/java/hh/fi/swd22/haheQuestions/web/KyselyController.java
+++ src/main/java/hh/fi/swd22/haheQuestions/web/KyselyController.java
@@ -1,23 +1,30 @@
 package hh.fi.swd22.haheQuestions.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import hh.fi.swd22.haheQuestions.domain.Kysely;
 import hh.fi.swd22.haheQuestions.domain.KyselyRepository;
 import hh.fi.swd22.haheQuestions.domain.KysymysRepository;
 
+// vr. Kyselyä Studenttiin ja Kysymystä Departmenttiin
+
 @Controller
-public class KysymysController {
+public class KyselyController {
 	@Autowired
 	private KyselyRepository kyselyrepository;
 	
-	@ResponseBody
-	@RequestMapping("/index")
-	public String hello() {
-		return "kokeilu";
-	}
+	@Autowired
+	private KysymysRepository kysymysrepository;
 	
+	@RequestMapping(value="/kyselyt", method= RequestMethod.GET)
+	public @ResponseBody List<Kysely> getKyselytRest() {
+		return (List<Kysely>) kyselyrepository.findAll();
+	}
+
 }
