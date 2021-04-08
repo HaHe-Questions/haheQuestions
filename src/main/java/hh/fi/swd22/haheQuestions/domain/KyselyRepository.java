1c799855f9d475db7c615c878dfa284d4eddc9d6
aleksiarpoo <aleksi.paunonen@myy.haaga-helia.fi>
2021-04-08 11:22:15 +0300

    kyselyrepo

---
+++ src/main/java/hh/fi/swd22/haheQuestions/domain/KyselyRepository.java
@@ -0,0 +1,7 @@
+package hh.fi.swd22.haheQuestions.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface KyselyRepository extends CrudRepository<Kysely, Long> {
+
+}
