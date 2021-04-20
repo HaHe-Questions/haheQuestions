8054986bf1b265afa3ca07cb5fd8826f071a5ba2
Emilia Heikonen <emilia.heikonen@myy.haaga-helia.fi>
2021-04-20 13:58:55 +0300

    uusikysymys.html

--- src/main/java/hh/fi/swd22/haheQuestions/web/KyselyController.java
+++ src/main/java/hh/fi/swd22/haheQuestions/web/KyselyController.java
@@ -1,10 +1,12 @@
 package hh.fi.swd22.haheQuestions.web;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -31,23 +33,43 @@ public class KyselyController {
 		return "kysely";
 	}
 	
+	// Rest palvelu, joka palauttaa kaikki kyselyt
 	@RequestMapping(value="/kyselyt", method= RequestMethod.GET)
 	public @ResponseBody List<Kysely> getKyselytRest() {
 		return (List<Kysely>) kyselyrepository.findAll();
 	}
 	
+	// Rest palvelu, joka palauttaa yhden kyselyn id:n perusteella
+    @RequestMapping(value="/kysely/{id}", method = RequestMethod.GET)
+    public @ResponseBody Optional<Kysely> findKyselyRest(@PathVariable("id") Long kId) {	
+    	return kyselyrepository.findById(kId);
+    } 
+	
 	// Lisää uusi kysely
 	@RequestMapping(value="/uusikysely")
 	public String uusiKysely(Model model) {
 		model.addAttribute("kysely", new Kysely());
-		model.addAttribute("kysymys", new Kysymys());
 		return "uusikysely";
 	}
 	
+	// Lisää uusi kysymys
+	@RequestMapping(value="/uusikysymys")
+	public String uusiKysymys(Model model) {
+		model.addAttribute("kysymys", new Kysymys());
+		model.addAttribute("kyselyt", kyselyrepository.findAll());
+		return "uusikysymys";
+	}
+	
 	// Tallenna uusi kysely
-	@RequestMapping(value="/tallenna", method = RequestMethod.POST)
-	public String tallenna(Kysely kysely, Kysymys kysymys) {
+	@RequestMapping(value="/tallennakysely", method = RequestMethod.POST)
+	public String tallennaKysely(Kysely kysely) {
 		kyselyrepository.save(kysely);
+		return "redirect:kyselylista";
+	}
+	
+	// Tallenna uusi kysymys
+	@RequestMapping(value="/tallennakysymys", method = RequestMethod.POST)
+	public String tallennaKysymys(Kysymys kysymys) {
 		kysymysrepository.save(kysymys);
 		return "redirect:kyselylista";
 	}
